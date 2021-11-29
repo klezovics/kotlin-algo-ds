79e3ae294c1f6bcc51d355f9a3083ac025fcaf27
arthur.klezovich <klezovich@gmail.com>
2021-11-29 11:38:38 +0100

    Add some DSs to add

--- src/test/kotlin/com/example/olympiad/KotlinDS.kt
+++ src/test/kotlin/com/example/olympiad/KotlinDS.kt
@@ -3,4 +3,12 @@ package com.example.olympiad
 // Workout for Kotlin data structures 
 class KotlinDS {
 
+    // Priority queue
+    // Heap
+    // Binary tree
+    // Stack
+    // Queue
+    // Sorting
+    // Reversal
+    // Linked list
 }
