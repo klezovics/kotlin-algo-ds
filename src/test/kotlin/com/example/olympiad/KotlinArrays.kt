16e55bd343cd6e58f83efdada47cbb7f143d427e
arthur.klezovich <klezovich@gmail.com>
2021-11-29 11:11:17 +0100

    Initial commit

---
+++ src/test/kotlin/com/example/olympiad/KotlinArrays.kt
@@ -0,0 +1,3 @@
+package com.example.olympiad
+
+class KotlinArrays
---
+++ src/test/kotlin/com/example/olympiad/KotlinDsTest.kt
@@ -0,0 +1,14 @@
+package com.example.olympiad
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class KotlinDsTest {
+
+    @Test
+    fun testSimple() {
+        var a = 2
+        var b = 2
+        assertEquals(4, a + b)
+    }
+}
