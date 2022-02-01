ff832a1fbf660618bcf764395bc957dbb06714e4
arthur.klezovich <klezovich@gmail.com>
2022-02-01 11:12:53 +0100

    Add DFS implementation

--- src/test/kotlin/com/example/google/MatrixBfsAndDfs.kt
+++ src/test/kotlin/com/example/google/MatrixBfsAndDfs.kt
@@ -6,20 +6,57 @@ class MatrixBfsAndDfs {
 
     @Test
     fun doMatrixBfs() {
-        doDfs(get5by5Matrix()).forEach {
+        doBfs(get5by5Matrix()).forEach {
             println(it)
         }
     }
 
     @Test
     fun doMatrixDfs() {
-        doBfs(get5by5Matrix()).forEach {
+        doDfs(get5by5Matrix()).forEach {
             println(it)
         }
     }
 
     fun doDfs(m: Array<IntArray>): List<Int> {
-        return mutableListOf()
+        val visited = mutableListOf<BooleanArray>()
+        m.forEach {
+            visited.add(BooleanArray(m[0].size) { false })
+        }
+
+        val traversal = mutableListOf<Int>()
+        dfs(row = 0, col = 0, m = m, visited = visited.toTypedArray(), traversal = traversal)
+        return traversal
+    }
+
+    fun dfs(
+        row: Int,
+        col: Int,
+        m: Array<IntArray>,
+        visited: Array<BooleanArray>,
+        traversal: MutableList<Int>
+    ) {
+        if (row < 0) return
+        if (col < 0) return
+        if (row >= m[0].size) return
+        if (col >= m.size) return
+        if (visited[row][col]) return
+
+        val directions = listOf(
+            intArrayOf(-1, 0), // up
+            intArrayOf(0, 1), // right
+            intArrayOf(1, 0), // down
+            intArrayOf(0, -1) // left
+        )
+
+        visited[row][col] = true
+        traversal.add(m[row][col])
+
+        for (ii in 0 until directions.size) {
+            val nX = row + directions[ii][0]
+            val nY = col + directions[ii][1]
+            dfs(row = nX, col = nY, m = m, visited = visited, traversal = traversal)
+        }
     }
 
     fun doBfs(m: Array<IntArray>): List<Int> {
